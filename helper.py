41d39a782c73d8a9ff605fc26360abb64c061c1a
Abhinav Jain <abhinav@Abhinavs-MacBook-Pro.local>
2022-05-17 01:44:32 -0700

    Adding apple_basic

--- helper.py
+++ helper.py
@@ -1,35 +1,40 @@
 from openalea.plantgl.all import NurbsCurve
+from openalea.lpy import Lsystem, newmodule
 from random import uniform
 
 def amplitude(x): return 2
 
 def cut_from(pruning_id, s, path = None):
-    """Cuts starting from index pruning_id until branch 
-        end signified by ']' or the entire subtrees if pruning_id starts from leader"""
-   
-    bracket_balance = 0
-    cut_num = pruning_id
-    s[cut_num].append("no cut")
-    cut_num += 1
-    pruning_id +=1
-    total_length = len(s)
-    while(pruning_id < total_length):
-        if s[cut_num].name == '[':
-            bracket_balance+=1
-        if s[cut_num].name == ']':
-            if bracket_balance == 0:
-                break
-            else:
-                bracket_balance-=1
-        del s[cut_num]
-        pruning_id+=1             # Insert new node cut at the end of cut
-    if path != None:
-        new_lsystem = Lsystem(path) #Figure out to include time in this
-        new_lsystem.axiom = s
-        return new_lsystem
-    s.insertAt(cut_num, newmodule("I(1, 0.05)"))
-    print(s)
+    """Check cut_string_from_manipulation for manual implementation"""
+    s.insertAt(pruning_id, newmodule('F'))
+    s.insertAt(pruning_id+1, newmodule('%'))
     return s
+
+def cut_using_string_manipulation(pruning_id, s, path = None):
+  """Cuts starting from index pruning_id until branch 
+        end signified by ']' or the entire subtrees if pruning_id starts from leader"""
+  bracket_balance = 0
+  cut_num = pruning_id
+  s[cut_num].append("no cut")
+  cut_num += 1
+  pruning_id +=1
+  total_length = len(s)
+  while(pruning_id < total_length):
+      if s[cut_num].name == '[':
+          bracket_balance+=1
+      if s[cut_num].name == ']':
+          if bracket_balance == 0:
+              break
+          else:
+              bracket_balance-=1
+      del s[cut_num]
+      pruning_id+=1             # Insert new node cut at the end of cut
+  if path != None:
+      new_lsystem = Lsystem(path) #Figure out to include time in this
+      new_lsystem.axiom = s
+      return new_lsystem
+  s.insertAt(cut_num, newmodule("I(1, 0.05)"))
+  return s
 
 def pruning_strategy(it, lstring):
   if((it+1)%8 != 0):  
